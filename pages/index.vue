28bd59155f6b6fa3183ea64ddfad2e413b1883af
hirokisatogit <49369555+hirokisatogit@users.noreply.github.com>
2021-01-09 17:20:05 +0900

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,11 @@
           <h1 class="title">
             Image Guardian
           </h1>
-          <carousel>
-            <div class="contents">
-              <div v-for="(ipfsHash, index) in this.ipfsHashs" :key="index" @mouseover="stopRotation" @mouseout="startRotation">
-                <img class="image" :src="'https://ipfs.io/ipfs/' + ipfsHash" >
-              </div>
-            </div>
-          </carousel>
+            <carousel class="contents" :per-page="1" :autoplay="true" :loop="true" :pagination-padding="5" :autoplay-timeout="4000">
+              <slide v-for="(ipfsHashUrl, index) in this.ipfsHashUrls" :key="index">
+                <img class="image" :src="ipfsHashUrl" >
+              </slide>
+            </carousel>
           <form @submit="onSubmit">
             <input type="file" @change="captureFile" />
             <input type="submit" id="click" />
@@ -29,76 +27,31 @@ import getWeb3 from "~/plugins/getWeb3.js";
 import ipfs from "~/plugins/ipfs.js";
 import web3 from "~/plugins/web3.js";
 // import VuePreviewer from '~/plugins/vue-image-previewer.js';
-
 export default {
-  data: { 
+  data() { 
+  return { 
   write: 0, // コントラクトから取得する数値 
-  ipfsHashs: [],
-  state: {
-    web3: null, 
-    accounts: null, 
-    contract: null,
-    buffer: '',
-    // instance: '',
-  },
+  ipfsHashUrls: [
+    require("https://ipfs.io/ipfs/ipfsHashUrl")
+  ],
+  buffer: '',
+  } 
 }, 
-
 methods: {
-
-  startRotation: function () {
-    this.timer = setInterval(this.next, this.speed);
-  },
-  stopRotation: function () {
-    clearTimeout(this.timer);
-    this.timer = null;
-  },
-
-  next: function () {
-    var current = this.current;
-    var next = current + 1;
-
-    if (next > this.slides.length - 1) {
-      next = 0;
-    }
-    this.current = next;
-    this.setActive(this.current);
-},
-  prev: function () {
-    var current = this.current;
-    var prev = current - 1;
-
-    if (prev < 0) {
-      prev = this.slides.length -1;
-    }
-
-    this.current = prev;
-    this.setActive(this.current);
-},
-
-    isActive: function (slide) {
-      return this.current === slide;
-    },
-    setActive: function (slide) {
-      this.current = slide;
-    },
 
   async captureFile(event) {
     event.preventDefault()      // preventDefault()はもしイベントがキャンセル可能だったら自動でキャンセルする
     const file = await event.target.files[0]; // event.target.files でサイズ、形式などのファイル情報を取得する      
     const reader = await new window.FileReader(); // FileReader はデータ読み込みを目的としたオブジェクト
-    // Vue.set(this.state.buffer, 'Buffer(reader.result)')
     reader.readAsArrayBuffer(file);  // readAsArrayBuffer() でfileオブジェクトを読み込む
-
     reader.onloadend = () => { // onloadend はデータ読み込み時に発生するイベントハンドラ
-      this.state.buffer = Buffer(reader.result);
+      this.buffer = Buffer(reader.result);
       resolve(event.target.result);
   }
 },
-
   async onSubmit() {
     event.preventDefault()
-
-    ipfs.files.add(this.state.buffer, async (error, result) => {
+    ipfs.files.add(this.buffer, async (error, result) => {
       if(error) {
         console.error(error)
         return
@@ -111,17 +64,15 @@ methods: {
       // return this.loadIpfsHash();
   })
 },
-
   loadIpfsHash: async function() {
     const length = await this.$contract.methods.arraylength().call()
     for (var i = 0; i < length; i++) {
-      const ipfsHash = await this.$contract.methods.IpfsHash(i).call()
+      const ipfsHashUrl = await this.$contract.methods.IpfsHash(i).call()
       // console.log(ipfsHash);
-      this.ipfsHashs.push(ipfsHash)
+      this.ipfsHashUrls.push(ipfsHashUrl)
     }
   }
 },
-
   components: {
     Carousel,
     Slide
@@ -132,39 +83,23 @@ methods: {
       await this.loadIpfsHash();
     }, 1)
 },
-  mounted: function () {
-    this.startRotation();
-  }
+
 }
 </script>
 
-<style lang="scss" scoped>
-
-// @import '~/assets/scss/base.scss';
-
-.VueCarousel-slide {
-
-}
+<style>
 
 .top {
   margin: 0 auto;
+  max-height: 300vh;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
-
 .title {
   font-family:
-    /* 'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial, */
     sans-serif;
   height: 250px;
   display: block;
@@ -173,18 +108,27 @@ methods: {
   color: #35495e;
   /* letter-spacing: 1px; */
 }
-
-  .contents {
-    padding-top: 15px;
+.contents {
+  overflow: hidden;
+  position: relative;
+  /* padding-top: calc(9 / 16 * 100%);
+  width: 50vw;
+  height: 100px; */
+  /* background-color: rgba(0, 0, 0, 0.5); */
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
 }
-
-  .image {
-    /* display: block; */
-    height: 100px;
-    width: 100px;
-    /* margin: auto; */
-    object-fit: cover;
+.image {
+  display: block;
+  /* height: 250px; */
+  /* width: 500px; */
+  /* margin: auto; */
+  position: center;
+  width: 100%;
+  height: 100%;
+  /* align-items: center; */
+  text-align: center;
+  object-fit: cover;
+  /* opacity: 0; */
+  /* transition: opacity 1s; */
 }
-
-
 </style>
